0de54fb5e6da7a17f238b1884762a3bdb1546e72
Nguyen Hung Manh <nm_hung93@yahoo.de>
2019-12-02 03:09:58 +0100

    tja

--- servlet-dev/src/main/data/DBHandler.java
+++ servlet-dev/src/main/data/DBHandler.java
@@ -1,4 +1,22 @@
 package data;
 
+import java.sql.*;
+
 public class DBHandler {
+
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+
+    public DBHandler() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+        } catch(Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+    }
+
+
 }
---
+++ servlet-dev/src/main/data/main.java
@@ -0,0 +1,9 @@
+package data;
+
+public class main {
+
+    public static void main(String[] args) {
+        DBHandler connect = new DBHandler();
+
+    }
+}
