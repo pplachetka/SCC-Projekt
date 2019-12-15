97218a97edd358d2c2d62e6960ff103974195de2
Hung Manh Nguyen <nm_ung93@yahoo.de>
2019-12-15 16:18:17 +0100

    changed varchar to nvarchar

--- mysql_sandbox/mysql/init/init_db.sql
+++ mysql_sandbox/mysql/init/init_db.sql
@@ -6,9 +6,9 @@ DROP TABLE IF EXISTS USER;
 CREATE TABLE scc.USER
 (   
 UserID INT NOT NULL AUTO_INCREMENT
-,Name VARCHAR(100) NOT NULL
-,FamilyName VARCHAR(100) NOT NULL
-,Password VARCHAR(100) NOT NULL
+,Name NVARCHAR(100) NOT NULL
+,FamilyName NVARCHAR(100) NOT NULL
+,Password NVARCHAR(100) NOT NULL
 ,isAdmin tinyint NOT NULL
 , PRIMARY KEY (UserId)
 
@@ -16,7 +16,7 @@ UserID INT NOT NULL AUTO_INCREMENT
 
 CREATE TABLE scc.MENUITEM (
     MenuItemID INT NOT NULL AUTO_INCREMENT
-    , Description VARCHAR(1000) NOT NULL
+    , Description NVARCHAR(1000) NOT NULL
     , Costs numeric(15,2)
     , PRIMARY KEY (MenuItemID)
 );
@@ -36,7 +36,7 @@ CREATE TABLE scc.USER_TOKEN (
 
 INSERT INTO USER (Name, FamilyName, Password, isAdmin)
 VALUES
-('admin', 'admin', 'admin', '1');
+('admin', 'admin', 'admin', '1'),('user','username', 'userfamily','0');
 
 INSERT INTO MENUITEM (Description, Costs)
 VALUES ('Huhn','3.5')
