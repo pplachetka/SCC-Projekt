70cec0bf5b41ccfc40f99d232faf5367571e6107
Nguyen Hung Manh <nm_hung93@yahoo.de>
2020-01-11 21:27:36 +0100

    setMenuItemSchedule now takes json object

--- speiseplan/src/main/java/com/scc/speiseplan/servlet/scheduleEndpoint.java
+++ speiseplan/src/main/java/com/scc/speiseplan/servlet/scheduleEndpoint.java
@@ -1,28 +1,64 @@
 package com.scc.speiseplan.servlet;
 
+import com.scc.speiseplan.data.MenuItemSchedule;
+import com.scc.speiseplan.data.MenuItemScheduleReceiver;
 import com.scc.speiseplan.data.MyDBHandler;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/schedule")
 public class scheduleEndpoint {
 
-
+/*
+    /* using jersey build in jackson lib to import json directly into POJO
+    * https://mkyong.com/webservices/jax-rs/restful-java-client-with-jersey-client/
+    * accepting a list of jsons
+    * https://stackoverflow.com/questions/32510240/how-to-accept-json-array-input-in-jersey-rest-webservice */
+/*
     @Path("/setMenuItemSchedule")
-    @GET
-    public Response setMenuItemSchedule (@QueryParam("date") int date,
-                                         @QueryParam("position") int position,
-                                         @QueryParam("menuItemID") int menuItemID) {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response setMenuItemSchedule (List<MenuItemSchedule> menuItemSchedule) {
+        */
+    @Path("/setMenuItemSchedule")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response setMenuItemSchedule (MenuItemScheduleReceiver menuItemScheduleReceiver) {
+        /* expecting: [{\"date\":\"20190101\",\"position\":\"1\",\"menuItemID\":\"2\"},{...}]";
+        * with dateformat: YYYYMMDD */
+
+        int token = menuItemScheduleReceiver.getToken();
+        ArrayList<MenuItemSchedule>  menuItemScheduleArray = menuItemScheduleReceiver.getMenuItemSchedule();
+
+        // ToDo: check for token
+        System.out.println(token);
+        for (MenuItemSchedule item : menuItemScheduleArray ) {
+            new MyDBHandler()
+                    .setMenuItemSchedule(
+                            item.getDate(),
+                            item.getMenuItemID(),
+                            item.getPosition()
+                    );
+        }
+
+
+
+        //System.out.println(menuItemSchedule.get(0).toString());
+
+
+        // müsste eig ein MenuItemSchedule
+
+
+
 
         //ToDo:Tokencheck => admin?
-        //ToDo: iterieren, es sollte ne json per post kommen
-        //dateformat: YYYYMMDD
-        System.out.println("date: " +date + "| position: " + position + "| menuItemID: "+menuItemID);
-        new MyDBHandler().setMenuItemSchedule(date, position, menuItemID);
         //ToDo: exception handling, inserted?
         //ToDO: typecast date as date?
 
